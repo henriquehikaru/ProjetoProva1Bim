70a68e7587aa10878efab52deaf625193bdfb532
henriquehikaru <henrique.hikaru@unesp.br>
2025-05-20 08:23:26 -0300

    Created the 'Imagens' folder with button images and updated the resource.resx to load them correctly.

--- ProjetoProva1Bim/frmMenu.Designer.cs
+++ ProjetoProva1Bim/frmMenu.Designer.cs
@@ -28,44 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnMedia = new System.Windows.Forms.Button();
-            this.btnTemperatura = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.lblTemp = new System.Windows.Forms.Label();
             this.lblMedia = new System.Windows.Forms.Label();
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.btnTemperatura = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnMedia
-            // 
-            this.btnMedia.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnMedia.Image = global::ProjetoProva1Bim.Properties.Resources.school_outline_256x222;
-            this.btnMedia.Location = new System.Drawing.Point(458, 208);
-            this.btnMedia.Name = "btnMedia";
-            this.btnMedia.Size = new System.Drawing.Size(300, 300);
-            this.btnMedia.TabIndex = 1;
-            this.btnMedia.Text = "&m";
-            this.btnMedia.UseVisualStyleBackColor = true;
-            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
-            // 
-            // btnTemperatura
-            // 
-            this.btnTemperatura.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnTemperatura.Image = global::ProjetoProva1Bim.Properties.Resources.temperature_max_256x256;
-            this.btnTemperatura.Location = new System.Drawing.Point(30, 208);
-            this.btnTemperatura.Name = "btnTemperatura";
-            this.btnTemperatura.Size = new System.Drawing.Size(300, 300);
-            this.btnTemperatura.TabIndex = 0;
-            this.btnTemperatura.Text = "&t";
-            this.btnTemperatura.UseVisualStyleBackColor = true;
-            this.btnTemperatura.Click += new System.EventHandler(this.btnTemperatura_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(148, 44);
+            this.label1.Location = new System.Drawing.Point(197, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(491, 31);
+            this.label1.Size = new System.Drawing.Size(617, 39);
             this.label1.TabIndex = 2;
             this.label1.Text = "Bem-vindo! Selecione uma Ferramenta:";
             // 
@@ -73,9 +50,10 @@
             // 
             this.lblTemp.AutoSize = true;
             this.lblTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTemp.Location = new System.Drawing.Point(38, 155);
+            this.lblTemp.Location = new System.Drawing.Point(51, 191);
+            this.lblTemp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTemp.Name = "lblTemp";
-            this.lblTemp.Size = new System.Drawing.Size(285, 50);
+            this.lblTemp.Size = new System.Drawing.Size(364, 58);
             this.lblTemp.TabIndex = 3;
             this.lblTemp.Text = "Acréscimo de Temperatura (%)\r\n\r\n";
             this.lblTemp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -84,23 +62,51 @@
             // 
             this.lblMedia.AutoSize = true;
             this.lblMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMedia.Location = new System.Drawing.Point(470, 155);
+            this.lblMedia.Location = new System.Drawing.Point(627, 191);
+            this.lblMedia.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMedia.Name = "lblMedia";
-            this.lblMedia.Size = new System.Drawing.Size(273, 25);
+            this.lblMedia.Size = new System.Drawing.Size(351, 29);
             this.lblMedia.TabIndex = 4;
             this.lblMedia.Text = "Calculadora de Média Escolar";
             // 
+            // btnMedia
+            // 
+            this.btnMedia.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnMedia.Image = global::ProjetoProva1Bim.Properties.Resources.school_outline_256x222;
+            this.btnMedia.Location = new System.Drawing.Point(611, 256);
+            this.btnMedia.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(400, 369);
+            this.btnMedia.TabIndex = 1;
+            this.btnMedia.Text = "&m                   ";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
+            // 
+            // btnTemperatura
+            // 
+            this.btnTemperatura.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnTemperatura.Image = global::ProjetoProva1Bim.Properties.Resources.temperature_max_256x256;
+            this.btnTemperatura.Location = new System.Drawing.Point(40, 256);
+            this.btnTemperatura.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTemperatura.Name = "btnTemperatura";
+            this.btnTemperatura.Size = new System.Drawing.Size(400, 369);
+            this.btnTemperatura.TabIndex = 0;
+            this.btnTemperatura.Text = "&t";
+            this.btnTemperatura.UseVisualStyleBackColor = true;
+            this.btnTemperatura.Click += new System.EventHandler(this.btnTemperatura_Click);
+            // 
             // frmMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(792, 541);
+            this.ClientSize = new System.Drawing.Size(1056, 666);
             this.Controls.Add(this.lblMedia);
             this.Controls.Add(this.lblTemp);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnMedia);
             this.Controls.Add(this.btnTemperatura);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMenu";
